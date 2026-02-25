80e38bdb06c7f4114853e37590c637719c0f2624
Cika <cikazhrasy@gmail.com>
2026-02-25 13:19:40 +0700

    main ui belum fixed

--- script/main_UI.gd
+++ script/main_UI.gd
@@ -1,5 +1,13 @@
 extends Control
 
-func _process(delta):
-	$dayPanel/dayLabel.text = "Hari " + str(GameManager.current_day)
-	$moneyPanel/moneyLabel.text = "💰 Rp " + str(GameManager.money)
+var hp_scene = preload("res://scene/ui_hp.tscn")
+var hp_instance = null
+
+func _process(_delta):
+	$rootUI/dayPanel/dayLabel.text = "Hari " + str(GameManager.current_day)
+	$rootUI/moneyPanel/moneyLabel.text = "💰 Rp " + str(GameManager.money)
+
+func _on_HpButton_pressed():
+	if hp_instance == null:
+		hp_instance = hp_scene.instance()
+		add_child(hp_instance)
