4042899c2ec52ef8481c90045341f24863bd3c9a
Cika <cikazhrasy@gmail.com>
2026-02-25 22:16:46 +0700

    main ui fixed, hp belum

--- script/main_UI.gd
+++ script/main_UI.gd
@@ -3,11 +3,20 @@ extends Control
 var hp_scene = preload("res://scene/ui_hp.tscn")
 var hp_instance = null
 
+func _ready():
+	update_ui()
+
 func _process(_delta):
-	$rootUI/dayPanel/dayLabel.text = "Hari " + str(GameManager.current_day)
-	$rootUI/moneyPanel/moneyLabel.text = "💰 Rp " + str(GameManager.money)
+	update_ui()
+
+func update_ui():
+	$dayPanel/dayLabel.text = "Hari " + str(GameManager.current_day)
+	$moneyPanel/moneyLabel.text = "💰 Rp " + str(GameManager.money)
 
 func _on_HpButton_pressed():
 	if hp_instance == null:
 		hp_instance = hp_scene.instance()
-		add_child(hp_instance)
+		get_parent().add_child(hp_instance)
+	else:
+		hp_instance.queue_free()
+		hp_instance = null
