69b553545787c35b6853eb38b58405112b031704
Cika <cikazhrasy@gmail.com>
2026-03-14 14:53:29 +0700

    idk man

--- script/mainMenu.gd
+++ script/mainMenu.gd
@@ -3,7 +3,7 @@ extends Control
 
 	
 func _on_Start_pressed():
-	get_tree().change_scene("res://scene/toko.tscn")
+	get_tree().change_scene("res://scene/rumah.tscn")
 
 func _on_Settings_pressed():
 	get_tree().change_scene("res://scene/settings.tscn")
